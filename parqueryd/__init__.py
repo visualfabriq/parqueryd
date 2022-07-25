1abf040a3047926f2877771d6001d23294a60b8d
Felix Finkbeiner <ffinkbeiner@visualfabriq.com>
2022-07-25 15:23:12 +0200

    Add version to init

--- parqueryd/__init__.py
+++ parqueryd/__init__.py
@@ -1,6 +1,9 @@
+import os
 import logging
 # from version import __version__
 
+pre_release_version = os.getenv('PRE_RELEASE_VERISION', '')
+__version__ = '0.1.55{}'.format(pre_release_version)
 
 # initalize logger
 logger = logging.getLogger('parqueryd')
