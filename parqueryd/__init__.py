20ad2b124cf1b62669c435322406f255871a26cf
franciscofabian <franciscofabian@me.com>
2024-07-10 13:53:59 +0200

    Remove comment

--- parqueryd/__init__.py
+++ parqueryd/__init__.py
@@ -1,6 +1,5 @@
 import os
 import logging
-# from version import __version__
 
 pre_release_version = os.getenv('PRE_RELEASE_VERSION', '')
 __version__ = pre_release_version if pre_release_version else '1.1.0'
