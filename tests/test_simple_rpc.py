e9ab6eee0a6578f20959e2a2ef2dcf94049ee04c
Jelle Verstraaten <jverstraaten@visualfabriq.com>
2024-07-10 11:47:12 +0200

    Fix import

--- tests/test_simple_rpc.py
+++ tests/test_simple_rpc.py
@@ -6,12 +6,11 @@ from time import sleep
 import pandas as pd
 import pytest
 import redis
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 from parquery.write import df_to_parquet
 
 import parqueryd.config
 from parqueryd.controller import ControllerNode
-from parqueryd.rpc import RPC
 from parqueryd.util import get_my_ip
 from parqueryd.worker import WorkerNode, DownloaderNode
 
